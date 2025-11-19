59a549e50223787018a5e3401cccfdc34beb3a47
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2025-11-18 19:27:06 -0600

    ADD: MainTopBar component

--- app/src/main/java/com/jaamcoding/apigames/ui/components/MainTopBar.kt
+++ app/src/main/java/com/jaamcoding/apigames/ui/components/MainTopBar.kt
@@ -2,13 +2,11 @@ package com.jaamcoding.apigames.ui.components
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
