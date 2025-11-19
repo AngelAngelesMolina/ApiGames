15a5f25d424f1e02f38a85522aee9b80cdfc4cf5
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2025-11-19 15:56:41 -0600

    ADD: Navigation to detail with id as parameter

--- app/src/main/java/com/jaamcoding/apigames/navigation/NavManager.kt
+++ app/src/main/java/com/jaamcoding/apigames/navigation/NavManager.kt
@@ -2,23 +2,29 @@ package com.jaamcoding.apigames.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHost
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.jaamcoding.apigames.ui.viewmodel.GamesViewModel
 import com.jaamcoding.apigames.ui.views.DetailView
 import com.jaamcoding.apigames.ui.views.HomeView
 
 @Composable
-fun NavManager(gamesVm : GamesViewModel) {
+fun NavManager(gamesVm: GamesViewModel) {
 
     val navController = rememberNavController()
-    NavHost(navController, startDestination ="Home"){
-        composable("Home"){
+    NavHost(navController, startDestination = "Home") {
+        composable("Home") {
             HomeView(gamesVm, navController)
         }
-        composable("DetailView"){
-            DetailView(gamesVm, navController)
+        composable(
+            "DetailView/{id}",
+            arguments = listOf(navArgument("id") { type = NavType.IntType })
+        ) {
+            val id = it.arguments?.getInt("id") ?: 0
+            DetailView(gamesVm, navController, id)
         }
 
     }
