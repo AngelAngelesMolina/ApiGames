2109759930a699e08d1e8367f253164246516cd7
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2025-11-18 19:06:39 -0600

    ADD: HomeView into MainActivity

--- app/src/main/java/com/jaamcoding/apigames/ui/MainActivity.kt
+++ app/src/main/java/com/jaamcoding/apigames/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,6 +13,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.jaamcoding.apigames.ui.theme.ApiGamesTheme
+import com.jaamcoding.apigames.ui.viewmodel.GamesViewModel
+import com.jaamcoding.apigames.ui.views.HomeView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -19,13 +22,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val viewModel : GamesViewModel by viewModels()
         setContent {
             ApiGamesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    HomeView(vm = viewModel, modifier = Modifier.padding(innerPadding))
                 }
             }
         }
--- app/src/main/java/com/jaamcoding/apigames/ui/views/HomeView.kt
+++ app/src/main/java/com/jaamcoding/apigames/ui/views/HomeView.kt
@@ -6,11 +6,14 @@ import androidx.compose.runtime.collectAsState
 import com.jaamcoding.apigames.ui.viewmodel.GamesViewModel
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 
 @Composable
-fun HomeView(vm: GamesViewModel) {
+fun HomeView(
+    vm: GamesViewModel, modifier: Modifier
+) {
     val games by vm.games.collectAsState()
-    LazyColumn {
+    LazyColumn(modifier = modifier) {
         items(games.count()) { game ->
             Text(text = games[game].name)
         }
