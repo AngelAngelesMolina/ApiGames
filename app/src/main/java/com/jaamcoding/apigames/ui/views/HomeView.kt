644b38f72e0bffc6758f14dd9fa846684121404e
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2025-11-19 15:41:37 -0600

    CardGame impl

--- app/src/main/java/com/jaamcoding/apigames/ui/views/HomeView.kt
+++ app/src/main/java/com/jaamcoding/apigames/ui/views/HomeView.kt
@@ -1,8 +1,10 @@
 package com.jaamcoding.apigames.ui.views
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.collectAsState
@@ -10,7 +12,12 @@ import com.jaamcoding.apigames.ui.viewmodel.GamesViewModel
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.jaamcoding.apigames.ui.components.CardGame
 import com.jaamcoding.apigames.ui.components.MainTopBar
+import com.jaamcoding.apigames.ui.theme.CustomBlack
 
 @Composable
 fun HomeView(
@@ -30,9 +37,16 @@ fun HomeView(
 @Composable
 fun ContentHomeView(vm: GamesViewModel, pad: PaddingValues) {
     val games by vm.games.collectAsState()
-    LazyColumn(modifier = Modifier.padding(pad)) {
-        items(games.count()) { game ->
-            Text(text = games[game].name)
+    LazyColumn(modifier = Modifier
+        .padding(pad)
+        .background(CustomBlack)) {
+        items(games) { game ->
+            CardGame(game, {})
+            Text(
+                text = game.name, fontWeight = FontWeight.ExtraBold,
+                color = Color.White,
+                modifier = Modifier.padding(start = 10.dp)
+            )
         }
     }
 }
